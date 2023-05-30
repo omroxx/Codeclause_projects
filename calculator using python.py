fa841cff115a4347e185a326ff4d93e35b6fba32
Om Pusadkar <130340319+omroxx@users.noreply.github.com>
2023-05-30 11:08:28 -0700

    Update calculator using python.py

--- calculator using python.py
+++ calculator using python.py
@@ -1,105 +1,105 @@
 from tkinter import *
-from unittest import result
 
+win = Tk() # This is to create a basic window
+win.geometry("312x324")  # this is for the size of the window 
+win.resizable(0, 0)  # this is to prevent from resizing the window
+win.title("Calculator")
 
-def buttonClick(number):
-    global operator
-    operator = operator + str(number)
-    input_value.set(operator)
+###################Starting with functions ####################
+# 'btn_click' function : 
+# This Function continuously updates the 
+# input field whenever you enter a number
 
+def btn_click(item):
+    global expression
+    expression = expression + str(item)
+    input_text.set(expression)
 
-def buttonClear():
-    global operator
-    operator = ""
-    input_value.set("")
+# 'bt_clear' function :This is used to clear 
+# the input field
 
-
-def buttonEqual():
-    global operator
-    result = str(eval(operator))
-    input_value.set(result)
-    operator = ""
-
-
-main = Tk()
-main.title("Calculator")
-
-operator = ""
-input_value = StringVar()
-display_text = Entry(main, font=("arial", 20, "bold"), textvariable=input_value, bd=30, insertwidth=4,
-                     bg="powder blue", justify=RIGHT)
-display_text.grid(columnspan=4)
-
-# Row 1 7 8 9 +
-
-btn_7 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="7", command=lambda: buttonClick(7))
-btn_7.grid(row=1, column=0)
-
-btn_8 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="8", command=lambda: buttonClick(8))
-btn_8.grid(row=1, column=1)
-
-btn_9 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="9", command=lambda: buttonClick(9))
-btn_9.grid(row=1, column=2)
-
-btn_add = Button(main, padx=16, bd=8, fg="black",
-                 font=("arial", 20, "bold"), text="+", command=lambda: buttonClick("+"))
-btn_add.grid(row=1, column=3)
-
-# Row 2 - 4 5 6 -
-
-btn_4 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="4", command=lambda: buttonClick(4))
-btn_4.grid(row=2, column=0)
-
-btn_5 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="5", command=lambda: buttonClick(5))
-btn_5.grid(row=2, column=1)
-
-btn_6 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="6", command=lambda: buttonClick(6))
-btn_6.grid(row=2, column=2)
-
-btn_sub = Button(main, padx=16, bd=8, fg="black",
-                 font=("arial", 20, "bold"), text="-", command=lambda: buttonClick("-"))
-btn_sub.grid(row=2, column=3)
-
-# Row 3 - 1 2 3 *
-
-btn_1 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="4", command=lambda: buttonClick(1))
-btn_1.grid(row=3, column=0)
-
-btn_2 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="5", command=lambda: buttonClick(2))
-btn_2.grid(row=3, column=1)
-
-btn_3 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="6", command=lambda: buttonClick(3))
-btn_3.grid(row=3, column=2)
-
-btn_mul = Button(main, padx=16, bd=8, fg="black",
-                 font=("arial", 20, "bold"), text="*", command=lambda: buttonClick("*"))
-btn_mul.grid(row=3, column=3)
-
-# Row 4 - 0 C = /
-
-btn_0 = Button(main, padx=16, bd=8, fg="black",
-               font=("arial", 20, "bold"), text="0", command=lambda: buttonClick(0))
-btn_0.grid(row=4, column=0)
-
-btn_clear = Button(main, padx=16, bd=8, fg="black",
-                   font=("arial", 20, "bold"), text="C", command=buttonClear)
-btn_clear.grid(row=4, column=1)
-
-btn_equal = Button(main, padx=16, bd=8, fg="black",
-                   font=("arial", 20, "bold"), text="=", command=buttonEqual)
-btn_equal.grid(row=4, column=2)
-
-btn_div = Button(main, padx=16, bd=8, fg="black",
-                 font=("arial", 20, "bold"), text="/", command=lambda: buttonClick("/"))
-btn_div.grid(row=4, column=3)
-
-main.mainloop()
+def bt_clear(): 
+    global expression 
+    expression = "" 
+    input_text.set("")
+ 
+# 'bt_equal':This method calculates the expression 
+# present in input field
+ 
+def bt_equal():
+    global expression
+    result = str(eval(expression)) # 'eval':This function is used to evaluates the string expression directly
+    input_text.set(result)
+    expression = ""
+ 
+expression = ""
+ 
+# 'StringVar()' :It is used to get the instance of input field
+ 
+input_text = StringVar()
+ 
+# Let us creating a frame for the input field
+ 
+input_frame = Frame(win, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=2)
+ 
+input_frame.pack(side=TOP)
+ 
+#Let us create a input field inside the 'Frame'
+ 
+input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT)
+ 
+input_field.grid(row=0, column=0)
+ 
+input_field.pack(ipady=10) # 'ipady' is internal padding to increase the height of input field
+ 
+#Let us creating another 'Frame' for the button below the 'input_frame'
+ 
+btns_frame = Frame(win, width=312, height=272.5, bg="grey")
+ 
+btns_frame.pack()
+ 
+# first row
+ 
+clear = Button(btns_frame, text = "C", fg = "black", width = 32, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_clear()).grid(row = 0, column = 0, columnspan = 3, padx = 1, pady = 1)
+ 
+divide = Button(btns_frame, text = "/", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("/")).grid(row = 0, column = 3, padx = 1, pady = 1)
+ 
+# second row
+ 
+seven = Button(btns_frame, text = "7", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(7)).grid(row = 1, column = 0, padx = 1, pady = 1)
+ 
+eight = Button(btns_frame, text = "8", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(8)).grid(row = 1, column = 1, padx = 1, pady = 1)
+ 
+nine = Button(btns_frame, text = "9", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(9)).grid(row = 1, column = 2, padx = 1, pady = 1)
+ 
+multiply = Button(btns_frame, text = "*", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("*")).grid(row = 1, column = 3, padx = 1, pady = 1)
+ 
+# third row
+ 
+four = Button(btns_frame, text = "4", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(4)).grid(row = 2, column = 0, padx = 1, pady = 1)
+ 
+five = Button(btns_frame, text = "5", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(5)).grid(row = 2, column = 1, padx = 1, pady = 1)
+ 
+six = Button(btns_frame, text = "6", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(6)).grid(row = 2, column = 2, padx = 1, pady = 1)
+ 
+minus = Button(btns_frame, text = "-", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("-")).grid(row = 2, column = 3, padx = 1, pady = 1)
+ 
+# fourth row
+ 
+one = Button(btns_frame, text = "1", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(1)).grid(row = 3, column = 0, padx = 1, pady = 1)
+ 
+two = Button(btns_frame, text = "2", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(2)).grid(row = 3, column = 1, padx = 1, pady = 1)
+ 
+three = Button(btns_frame, text = "3", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(3)).grid(row = 3, column = 2, padx = 1, pady = 1)
+ 
+plus = Button(btns_frame, text = "+", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("+")).grid(row = 3, column = 3, padx = 1, pady = 1)
+ 
+# fourth row
+ 
+zero = Button(btns_frame, text = "0", fg = "black", width = 21, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 4, column = 0, columnspan = 2, padx = 1, pady = 1)
+ 
+point = Button(btns_frame, text = ".", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click(".")).grid(row = 4, column = 2, padx = 1, pady = 1)
+ 
+equals = Button(btns_frame, text = "=", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_equal()).grid(row = 4, column = 3, padx = 1, pady = 1)
+ 
+win.mainloop()
